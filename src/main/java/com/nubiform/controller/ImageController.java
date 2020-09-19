f76d78be6d98c00f2a5f184bd588c4654ce96cba
nubiform <nubiform@localhost>
2020-09-19 15:39:46 +0900

    commit imagecontroller

--- src/main/java/com/nubiform/controller/ImageController.java
+++ src/main/java/com/nubiform/controller/ImageController.java
@@ -3,47 +3,57 @@ package com.nubiform.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nubiform.service.ImageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
+@RequestMapping("/api")
 public class ImageController {
 	
-	@Autowired
-	ImageService imageService;
+	private ImageService imageService;
 	
-	@RequestMapping("/getDirectories")
+	public ImageController(ImageService imageService) {
+		this.imageService = imageService;
+	}
+	
+	@GetMapping("/getDirectories")
 	public ArrayList<String> getDirectorys() {
 		return imageService.getDirectorys();
 	}
 	
-	@RequestMapping(value = "/getImage", produces = MediaType.IMAGE_JPEG_VALUE)
-	public @ResponseBody byte[] getImage(@RequestParam("id") String imageName) {
+	@GetMapping(value = "/getImage", produces = MediaType.IMAGE_JPEG_VALUE)
+	@Operation(description = "get image")
+	public byte[] getImage(@RequestParam("id") String imageName) {
 		return imageService.getImage(imageName);
 	}
 	
-	@RequestMapping(value = "/getImageThumb", produces = MediaType.IMAGE_JPEG_VALUE)
-	public @ResponseBody byte[] getImageThumb(@RequestParam("id") String imageName) {
+	@GetMapping(value = "/getImageThumb", produces = MediaType.IMAGE_JPEG_VALUE)
+	@Operation(description = "get thumbnail image")
+	public byte[] getImageThumb(@RequestParam("id") String imageName) {
 		return imageService.getImageThumb(imageName);
 	}
 	
-	@RequestMapping("/getMetadata")
+	@GetMapping("/getMetadata")
+	@Operation(description = "get image metadata")
 	public HashMap<Object, HashMap<Object, Object>> getMetadata(@RequestParam(value = "id", required=true) String imageName) {
 		return imageService.getMetadata(imageName);
 	}
 	
-	@RequestMapping("/getMetadataByKey")
+	@GetMapping("/getMetadataByKey")
+	@Operation(description = "get image metadata by key")
 	public HashMap<Object, Object> getMetadata(@RequestParam(value = "id", required=true) String imageName, @RequestParam("key") String key) {
 		return imageService.getMetadata(imageName, key);
 	}
 	
-	@RequestMapping("/getMetadataByKeys")
+	@GetMapping("/getMetadataByKeys")
+	@Operation(description = "get image metadata by key1, key2")
 	public String getMetadata(@RequestParam(value = "id", required=true) String imageName, @RequestParam("key1") String key1, @RequestParam("key2") String key2) {
 		return imageService.getMetadata(imageName, key1, key2);
 	}
